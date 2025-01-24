ff5c24320ef3b51903fceb03775b75c1bf3ce973
Craigryy <harria.hj@gmail.com>
2025-01-24 21:49:03 +0100

    debugging  port 8000

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -160,7 +160,7 @@ resource "aws_security_group" "ecs_service" {
   egress {
     from_port = 443
     to_port   = 443
-    protocol  = "-1"
+    protocol  = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"
     ]
