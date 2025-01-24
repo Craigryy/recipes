8e830bb9a3850244155628be5b9b6a63b2b0e0b0
Craigryy <harria.hj@gmail.com>
2025-01-24 13:17:37 +0100

    updated port to 8000

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -111,8 +111,8 @@ resource "aws_ecs_task_definition" "api" {
         user              = "nginx"
         portMappings = [
           {
-            containerPort = 8090
-            hostPort      = 8090
+            containerPort = 8000
+            hostPort      = 8000
           }
         ]
         environment = [
@@ -176,8 +176,8 @@ resource "aws_security_group" "ecs_service" {
 
   # HTTP inbound access
   ingress {
-    from_port   = 8090
-    to_port     = 8090
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
