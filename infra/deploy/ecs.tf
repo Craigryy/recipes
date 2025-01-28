e2fa358ecc9de5a4ace1429ebba4a72614fd18af
Craigryy <harria.hj@gmail.com>
2025-01-28 16:55:21 +0100

    added load balancer security group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -176,10 +176,12 @@ resource "aws_security_group" "ecs_service" {
 
   # HTTP inbound access
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.lb.id
+    ]
   }
 }
 
